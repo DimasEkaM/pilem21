d2eead535b503436ec020a8e0245f13809f86923
Dimas <mahendradimas21@gmail.com>
2019-09-18 15:49:35 +0700

    movie api

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function searchMovie(){
+	$('#movie-list').html('');
+//$.getJSON('http://omdbapi.com?apikey=9409874c&')
+
+	$.ajax({
+		url: 'http://omdbapi.com',
+		type:'get',
+		dataType: 'json',
+		data: {
+			'apikey' : '9409874c',
+			's' : $('#search-input').val()
+		},
+		success: function(result){
+			
+			if (result.Response == "True"){
+				let movies = result.Search;
+				$.each(movies, function(i,data){
+					$('#movie-list').append(`
+			<div class="col-md-4">
+			<div class="card mb-3" style="width: 18rem;">
+			  <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <h5 class="card-title">`+ data.Title +`</h5>
+			    <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+			    <a href="#" class="card-link">See Detail</a>
+
+			  </div>
+			</div>					
+			</div>			
+						`);
+				});
+
+
+			$('#search-input').val('');	
+
+			} else{
+				$('#movie-list').html(`
+					<div class="col">
+					<h1 class="text-center">` + result.Error + `</h1>
+					</div>
+					`)
+			}
+		}
+	});
+}
+
+
+
+$('#search-button').on('click',function(){
+	searchMovie();
+
+});
+
+$('#search-input').on('keyup',function(e){
+	if(e.which == 13){
+		searchMovie();
+	}
+})
